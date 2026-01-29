845aa8ae8c12249e1dc7334ee865a05e3281d1d3
Varshitha <varshitha9408@gmail.com>
2026-01-29 23:18:26 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+a=25
+b=2.9
+c="hi"
+d=True
+e=[1,2,3,"apple"]
+f=(25,92)
+g={ "name": "john","age":30}
+h={1,2,3}
+print (type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+
+

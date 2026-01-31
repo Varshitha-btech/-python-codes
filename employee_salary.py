4473a3334d8682c86a9d657f18964c524ac06a98
Varshitha <varshitha9408@gmail.com>
2026-01-31 22:37:43 +0530

    Add files via upload

---
+++ employee_salary.py
@@ -0,0 +1,7 @@
+Basicsalary=int(input("enter your Basicsalary: "))
+HRA=20/100*Basicsalary
+DA=105/100*Basicsalary
+Bonus=1000
+otherallowences=1300
+totalsalary=HRA+DA+Bonus+otherallowences
+print(totalsalary)

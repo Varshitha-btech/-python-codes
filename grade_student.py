c1dbb346156f64139df1cc62aa12f62b57b4a5fd
Varshitha <varshitha9408@gmail.com>
2026-02-04 22:31:36 +0530

    Add files via upload

---
+++ grade_student.py
@@ -0,0 +1,13 @@
+score=float(input("enter a score: "))
+if(score>=0.9):
+    print("grade is 'a'")
+elif(score>=0.8):
+    print("grade is'b'")
+elif(score>=0.7):
+    print("grade is 'c'")
+elif(score>=0.6):
+    print("grade is 'd'")
+elif(score<=0.6):
+    print("grade ic'f'")
+else:
+    print("an error")
